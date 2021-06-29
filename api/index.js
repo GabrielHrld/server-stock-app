1398dfa9da9382b6db61d85defe517fb8bea298d
Gabi <g.federico.rodriguez@gmail.com>
2021-06-28 22:41:05 -0300

    initial server

---
+++ api/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const helmet = require("helmet");
+require("dotenv").config();
+const chalk = require("chalk");
+const user = require("./components/users");
+
+const errors = require("../utils/errors");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//MIDDLEWARES
+app.use(express.json());
+app.use(helmet());
+
+//ROUTER
+app.use("/api/user", user);
+// app.use("/api/auth", auth);
+app.use(errors);
+
+app.listen(PORT, () => {
+  console.log(
+    chalk.black.bgGreenBright(`Server listening in http://localhost:${PORT}`)
+  );
+});
